6790ca9835867c4c37ac8bf040e00a29f85c787e
gershinho <kpatel6607@gmail.com>
2025-10-13 13:07:01 -0500

    dunzo

--- personal_site_flask/app.py
+++ personal_site_flask/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 from data import AWARDS, TESTIMONIALS, PROJECTS
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
+app.url_map.strict_slashes = False
 
 @app.context_processor
 def inject_now():
@@ -14,15 +14,15 @@ def inject_now():
 def home():
     return render_template("home.html", testimonials=TESTIMONIALS)
 
-@app.route("/awards")
+@app.route("/awards/")
 def awards():
     return render_template("awards.html", awards=AWARDS)
 
-@app.route("/testimonials")
+@app.route("/testimonials/")
 def testimonials():
     return render_template("testimonials.html", testimonials=TESTIMONIALS)
 
-@app.route("/mywork")
+@app.route("/mywork/")
 def projects():
     return render_template("mywork.html", projects=PROJECTS)
 
